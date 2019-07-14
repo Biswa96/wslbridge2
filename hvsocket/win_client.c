aaf484b727ce689b5fbb00b61bbd34be152fa923
Biswa96 <nathbappai@gmail.com>
2019-07-14 14:54:33 +0530

    Add reverse hvsocket sample

---
+++ hvsocket/win_client.c
@@ -0,0 +1,71 @@
+/* 
+* This file is part of wslbridge2 project
+* Licensed under the GNU General Public License version 3
+*/
+
+#include <winsock2.h>
+#include "hvsocket.h"
+#include <stdio.h>
+
+#ifndef AF_HYPERV
+#define AF_HYPERV 34
+#endif
+
+#define BUFF_SIZE 400
+
+void Log(int ret, const char* function)
+{
+    if (ret == 0)
+        printf("%s success\n", function);
+    else
+        printf("%s error: %d\n", function, WSAGetLastError());
+}
+
+int main(void)
+{
+    struct WSAData wdata;
+    int ret = WSAStartup(MAKEWORD(2,2), &wdata);
+    if (ret != 0)
+        printf("WSAStartup error: %d\n", ret);
+
+    SOCKET Sock = socket(AF_HYPERV, SOCK_STREAM, HV_PROTOCOL_RAW);
+    if (Sock > 0)
+        printf("server socket: %lld\n", Sock);
+    else
+        printf("socket error: %d\n", WSAGetLastError());
+
+    struct _SOCKADDR_HV addr;
+    memset(&addr, 0, sizeof addr);
+
+    addr.Family = AF_HYPERV;
+    GUID VmId = { 0 }; /* enter the last parameter (a GUID) of wslhost.exe process */
+    memcpy(&addr.VmId, &VmId, sizeof addr.VmId);
+    memcpy(&addr.ServiceId, &HV_GUID_VSOCK_TEMPLATE, sizeof addr.ServiceId);
+
+    printf("Enter port number: ");
+    unsigned long Port = 0;
+    scanf("%lu", &Port);
+    addr.ServiceId.Data1 = Port;
+    ret = connect(Sock, (struct sockaddr*)&addr, sizeof addr);
+    Log(ret, "connect");
+
+    char msg[BUFF_SIZE];
+
+    while (1)
+    {
+        memset(msg, 0, sizeof msg);
+        printf("Enter message: ");
+        scanf("%s", msg);
+        ret = send(Sock, msg, strlen(msg), 0);
+        if (ret < 0)
+        {
+            printf("send error: %d\n", WSAGetLastError());
+            break;
+        }
+    }
+
+    /* cleanup */
+    if (Sock > 0)
+        closesocket(Sock);
+    return 0;
+}
---
+++ hvsocket/wsl_server.c
@@ -0,0 +1,75 @@
+/* 
+* This file is part of wslbridge2 project
+* Licensed under the GNU General Public License version 3
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/vm_sockets.h>
+
+#ifndef AF_VSOCK
+#define AF_VSOCK 40
+#endif
+
+#define BUFF_SIZE 400
+
+int main(void)
+{
+    int sockfd = socket(AF_VSOCK, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        printf("socket error: %s\n", strerror(errno));
+    else
+        printf("socket: %d\n", sockfd);
+
+    struct sockaddr_vm addr;
+    memset(&addr, 0, sizeof addr);
+
+    addr.svm_family = AF_VSOCK;
+    addr.svm_port = VMADDR_PORT_ANY;
+    addr.svm_cid = VMADDR_CID_ANY;
+    int ret = bind(sockfd, (struct sockaddr*)&addr, sizeof addr);
+    if (ret < 0)
+        printf("bind error: %s\n", strerror(errno));
+
+    socklen_t addrlen = sizeof addr;
+    ret = getsockname(sockfd, (struct sockaddr*)&addr, &addrlen);
+    if (ret < 0)
+        printf("getsockname error: %s\n", strerror(errno));
+    else
+        printf("getsockname port: %d\n", addr.svm_port);
+
+    ret = listen(sockfd, 1);
+    if (ret < 0)
+        printf("listen error: %s\n", strerror(errno));
+
+    int csockfd = accept(sockfd, (struct sockaddr*)&addr, &addrlen);
+    if (csockfd < 0)
+        printf("accept error: %s\n", strerror(errno));
+    else
+        printf("client socket: %d\n", csockfd);
+
+    char msg[BUFF_SIZE];
+
+    do
+    {
+        memset(msg, 0, sizeof msg);
+        ret = recv(csockfd, msg, BUFF_SIZE, 0);
+        if(ret > 0)
+            printf("%s\n", msg);
+        else if (ret == 0)
+            printf("server closing...\n");
+        else
+            break;
+
+    } while (ret > 0);
+
+    /* cleanup */
+    if (sockfd > 0)
+        close(sockfd);
+    if (csockfd > 0)
+        close(csockfd);
+    return 0;
+}
